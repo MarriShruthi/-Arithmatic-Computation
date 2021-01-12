51972a8674bf6c23e85ec2791f2a107c7406b507
Shruthi <shruthimarri97@gmail.com>
2021-01-12 18:58:34 +0530

    Added: Arithmatic computation storing in dictionary

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,6 +4,22 @@ read -p "Enter the value of a :: " a
 read -p "Enter the value of b :: " b
 read -p "Enter the value of c :: " c
 
-result=$(( $a % $b + $c ))
-echo $result
+declare -A result
+
+result1=$(( $a + $b * $c ))
+result[1]=$result1
+
+result2=$(( $a * $b + $c ))
+result[2]=$result2
+
+result3=$(( $c + $a / $b ))
+result[3]=$result3
+
+result4=$(( $a % $b + $c ))
+result[4]=$result4
+
+echo "All results ::" ${result[@]}
+
+
+
 
