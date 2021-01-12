78286901eba28e19ff91b14f20af58064074572d
Shruthi <shruthimarri97@gmail.com>
2021-01-12 18:23:30 +0530

    Added: Arithmatic computation 3

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,6 +4,6 @@ read -p "Enter the value of a :: " a
 read -p "Enter the value of b :: " b
 read -p "Enter the value of c :: " c
 
-result=$(( $a * $b + $c ))
+result=$(( $c + $a / $b ))
 echo $result
 
