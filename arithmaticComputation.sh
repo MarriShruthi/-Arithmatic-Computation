2cfece2ef05bb6c9cb75821940091d9caa8e159e
Shruthi <shruthimarri97@gmail.com>
2021-01-12 17:50:28 +0530

    Added: take three the inputs

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,1 +1,7 @@
-#welcome to  Arithmatic-Computation and Sorting
+#!/bin/bash -x
+
+read -p "Enter the value of a :: " a
+read -p "Enter the value of b :: " b
+read -p "Enter the value of c :: " c
+echo $a $b $c
+
