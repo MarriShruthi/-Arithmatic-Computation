2f1c4a33ba855bc90f8051503de08baab8c29bb5
Shruthi <shruthimarri97@gmail.com>
2021-01-12 19:21:16 +0530

    Added: values from dictionary to array

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,25 +1,15 @@
-#!/bin/bash -x
-
-read -p "Enter the value of a :: " a
-read -p "Enter the value of b :: " b
-read -p "Enter the value of c :: " c
+#!/bin/bash -x 
 
 declare -A result
 
-result1=$(( $a + $b * $c ))
-result[1]=$result1
+result[1]="2"
+result[2]="4"
+result[3]="6"
+result[4]="8"
 
-result2=$(( $a * $b + $c ))
-result[2]=$result2
+arr=( ${result[1]} ${result[2]} ${result[3]} ${result[4]} )
 
-result3=$(( $c + $a / $b ))
-result[3]=$result3
-
-result4=$(( $a % $b + $c ))
-result[4]=$result4
-
-echo "All results ::" ${result[@]}
-
+echo ${arr[@]}
 
 
 
